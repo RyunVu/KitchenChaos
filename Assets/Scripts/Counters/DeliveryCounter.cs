38634dc98e0bbd2230dd1b9e5cfb10339011d218
RyunVu <ryun3980@gmail.com>
2023-02-22 10:59:23 +0700

    finish delivery UI

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -7,6 +7,11 @@ public class DeliveryCounter : BaseCounter
     public override void Interact(Player player) {
         if (player.HasKitchenObject()) {
             if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                // Only accepts Plates
+
+                DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
+
+
                 player.GetKitchenObject().DestroySelf();
             }
         }
